df46fc7e74e73bda2ce7fa6d2efbfc7d0ca423d2
Mike Skalnik <mike.skalnik@gmail.com>
2011-09-19 10:36:33 -0400

    [#18078143] Add Gemlock#lockfile and locked_gemfile_specs to retrieve lockfile location and specs of all requested gems

--- lib/gemlock.rb
+++ lib/gemlock.rb
@@ -1,5 +1,24 @@
 require "gemlock/version"
 
 module Gemlock
-  # Your code goes here...
+  require 'gemlock/railtie' if defined?(Rails)
+
+  class << self
+    def lockfile
+      @lockfile = if defined?(Rails)
+                    Rails.root.join('Gemfile.lock')
+                  else
+                    Bundler::SharedHelpers.default_lockfile
+                  end
+    end
+
+    def locked_gemfile_specs
+      locked_content = Bundler.read_file(lockfile)
+      locked = Bundler::LockfileParser.new(locked_content)
+
+      gemfile_names = locked.dependencies.map(&:name)
+      locked_gemfile_specs = locked.specs.clone
+      locked_gemfile_specs.delete_if { |spec| !gemfile_names.include?(spec.name) }
+    end
+  end
 end
