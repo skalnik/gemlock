b606634a60044ec152b9f48f7651506192f622a2
Brian Gardner <bgardner7@gatech.edu>
2011-09-22 21:09:57 -0400

    Added gemlock tests

--- spec/gemlock_spec.rb
+++ spec/gemlock_spec.rb
@@ -2,6 +2,24 @@ require 'spec_helper'
 
 describe "gemlock methods" do
   it "should check if gems are outdated" do
+    Gemlock.stubs(:lockfile).returns((File.join(File.dirname(__FILE__), 'fixtures', 'Gemfile.lock')))
+    VCR.use_cassette('rails_cassette') do
+      request = Gemlock.request_gem_data("rails")
+    end 
+    expected = {"name"=>"rails",
+                "version"=>"3.0.1"} 
+    request["version"].should be > expected["version"]
+  end 
 
-  end
+  it "should request data from rubygems.org and return a hash" do
+    VCR.use_cassette('rails_cassette') do
+      request = Gemlock.request_gem_data("rails")
+    end 
+    expected ={ "name"=>"rails", 
+                "version"=>"3.1.0", 
+                "wiki_uri"=>"http://wiki.rubyonrails.org"}  
+
+    request["name"].should eql expected["name"]
+    request["version"].should eql expected["version"]
+  end 
 end
