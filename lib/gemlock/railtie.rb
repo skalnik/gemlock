595c37f55bbdd81cf8900739b28cc230ee05f6fd
Mike Skalnik <mike.skalnik@gmail.com>
2011-10-19 10:18:19 -0400

    [#18078591] Add simple rails initializer

--- lib/gemlock/railtie.rb
+++ lib/gemlock/railtie.rb
@@ -6,5 +6,22 @@ module Gemlock
     rake_tasks do
       load 'tasks/gemlock.rake'
     end
+
+    initializer 'Check for updates using Gemlock' do
+      puts "Checking for gem updates..."
+      outdated = Gemlock.outdated
+      if outdated.empty?
+        puts "All gems up to date!"
+      else
+        outdated.each_pair do |name, versions|
+          puts "#{name} is out of date!"
+          puts "Installed version: #{versions[:current]}. Latest version: #{versions[:latest]}"
+          puts "To update: bundle update #{name}"
+        end
+        puts ""
+        puts "To update all your gems via bundler:"
+        puts "bundle update"
+      end
+    end
   end
 end
